4caa5a70f16fdfaaafaa35e8784dfd4bc5fba4bf
李光春 <gc@dtapp.net>
2022-09-28 15:25:00 +0800

    - update

--- client.go
+++ client.go
@@ -7,17 +7,13 @@ import (
 
 // ClientConfig 实例配置
 type ClientConfig struct {
-	CustomerId       int                // 商家编号
-	CustomerKey      string             // 商家密钥
-	ApiGormClientFun golog.ApiClientFun // 日志配置
-	Debug            bool               // 日志开关
-	ZapLog           *golog.ZapLog      // 日志服务
+	CustomerId  int    // 商家编号
+	CustomerKey string // 商家密钥
 }
 
 // Client 实例
 type Client struct {
 	requestClient *gorequest.App // 请求服务
-	zapLog        *golog.ZapLog  // 日志服务
 	config        struct {
 		customerId  int    // 商家编号
 		customerKey string // 商家密钥
@@ -33,18 +29,10 @@ func NewClient(config *ClientConfig) (*Client, error) {
 
 	c := &Client{}
 
-	c.zapLog = config.ZapLog
-
 	c.config.customerId = config.CustomerId
 	c.config.customerKey = config.CustomerKey
 
 	c.requestClient = gorequest.NewHttp()
-
-	apiGormClient := config.ApiGormClientFun()
-	if apiGormClient != nil {
-		c.log.client = apiGormClient
-		c.log.status = true
-	}
 
 	return c, nil
 }
